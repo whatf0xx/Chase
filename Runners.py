e120dbc20deaad53518884a11ec71666512b9300
whatf0xx <whatf0xx@gmail.com>
2020-04-06 22:56:24 +0100

    basic ai works, but also not really

--- Runners.py
+++ Runners.py
@@ -7,7 +7,7 @@ Created on Tue Mar 24 12:59:10 2020
 Class code for the in-game runners
 """
 
-import pylab as pl
+import matplotlib.pyplot as plt
 import numpy as np
 
 class Runner():
@@ -19,7 +19,7 @@ class Runner():
         self._pos = np.array(pos)
         self._vel = np.array(vel)
         self._radius = radius
-        self._patch = pl.Circle(self._pos, radius, fc=colour)
+        self._patch = plt.Circle(self._pos, radius, fc=colour)
         
     def get_patch(self):
         """
@@ -33,6 +33,19 @@ class Runner():
         
     def setvel(self, newvel=[0,0]):
         self._vel = np.array(newvel)
+        
+    def seek(self, other):
+        """
+        Returns unit vector pointing from self to other.
+        """
+        direction = other._pos - self._pos
+        absolute = np.sqrt(direction[0]**2 + direction[1]**2)
+        return (direction/absolute)
+    
+    def separation(self, other):
+        return np.sqrt(
+                    (self._pos[0]-other._pos[0])**2
+                    + (self._pos[0]-other._pos[0])**2)
 
 class Chaser(Runner):
     """
@@ -42,10 +55,8 @@ class Chaser(Runner):
     def __init__(self, cpos, cvel):
         Runner.__init__(self, pos=cpos, vel=cvel, colour='red')
         
-    def seek(self, other):
-        direction = other._pos - self._pos
-        absolute = np.sqrt(direction[0]**2 + direction[1]**2)
-        self.setvel(direction/absolute)
+    def follow(self, other):
+        self.setvel(self.seek(other))
         
 class Scaper(Runner):
     """
@@ -53,7 +64,51 @@ class Scaper(Runner):
     They also have to avoid the walls.
     """
     def __init__(self, spos, svel):
-        Runner.__init__(self, )
+        Runner.__init__(self, pos=spos, vel=svel, colour='blue')
+        
+    def trackback(self, other):
+        x = np.linspace(self._pos[0], other._pos[0], 10)
+        y = np.linspace(self._pos[1], other._pos[1], 10)        
+        plt.plot(x, y)
+    
+    def isotropic_lines(self, other, n=1):
+        r = self.seek(other)
+        rotate = np.array([[np.cos(2*np.pi/(n+1)), -np.sin(2*np.pi/(n+1))],
+                            [np.sin(2*np.pi/(n+1)), np.cos(2*np.pi/(n+1))]])
+        for i in range(0, n):
+            r = np.matmul(rotate, r)
+            x0 = self._pos[0]
+            y0 = self._pos[1]
+            mu = []
+            mu.append((10-y0)/r[1])
+            mu.append((-10-y0)/r[1])
+            mu.append((10-x0)/r[0])
+            mu.append((-10-x0)/r[0])
+            
+            mu_chosen = 10
+            for j in range(0, 4):
+               if(mu[j] > 0 and mu[j] < mu_chosen):
+                   mu_chosen = mu[j]
+    
+            x = np.linspace(x0, (x0+mu_chosen*r[0]), 10)
+            y = np.linspace(y0, (y0+mu_chosen*r[1]), 10)
+            plt.plot(x, y)
+            
+    def flee(self, other, xlim, ylim):
+        x_wall = self._pos[0]/(xlim-self._pos[0]**2)**4
+        x_chase = 1/(other._pos[0]-self._pos[0])**5
+#        x_wall = 0 #for testing
+        x_slope = x_wall+x_chase
+        
+        y_wall = self._pos[1]/(ylim-self._pos[1]**2)**4
+        y_chase = 1/(other._pos[1]-self._pos[1])**5
+#        y_wall = 0 #for testing
+        y_slope = y_wall+y_chase
+        
+        mag = np.sqrt(x_slope**2 + y_slope**2)
+        newvel = np.array([-x_slope, -y_slope])
+        
+        self.setvel(newvel/mag)
 
 class World():
     """
@@ -63,33 +118,42 @@ class World():
                  [[0,0], [0,0], [0,0], [0,0]]):
         self._lenx = x
         self._leny = y
-        self._dt = dt
-        self._chaser = Chaser(cpos=start[0], cvel=start[1])
-        self._runner = Runner(pos=start[2], vel=start[3])
-        
-    def create(self):
-        ax = pl.axes(xlim=(-self._lenx/2, self._lenx/2),
+        self._dt = dt    
+        self._scaper = Scaper(spos=start[0], svel=start[1])    
+        self._chaser = Chaser(cpos=start[2], cvel=start[3])
+    
+    def create(self, first_frame=False, definition=1):
+        ax = plt.axes(xlim=(-self._lenx/2, self._lenx/2),
                                 ylim=(-self._leny/2, self._leny/2),
                                 aspect=1)
         ax.add_artist(self._chaser.get_patch())
-        ax.add_artist(self._runner.get_patch())
-        while(True):
-            pl.pause(self._dt)
-            self._chaser.seek(self._runner)
-            self._runner.move(self._dt)
+        ax.add_artist(self._scaper.get_patch())
+        
+        if(first_frame == True):
+            self._scaper.trackback(self._chaser)
+            self._scaper.isotropic_lines(self._chaser, definition)
+        
+        while(first_frame == False):
+            plt.pause(self._dt)
+            
+            self._chaser.follow(self._scaper)
+#            print("{:.3f}".format(self._scaper.separation(self._chaser)))
+            self._scaper.flee(self._chaser,
+                              self._lenx/2, self._leny/2)
+            
+            self._scaper.move(self._dt)
             self._chaser.move(self._dt)
-            ax.add_patch(self._runner.get_patch())
+            
+            ax.add_patch(self._scaper.get_patch())
+            #ax.add_patch(self._scaper.trackback(self._chaser))
             ax.add_patch(self._chaser.get_patch())
             
-            separation = np.sqrt(
-                    (self._chaser._pos[0]-self._runner._pos[0])**2
-                    + (self._chaser._pos[0]-self._runner._pos[0])**2)
-            
-            if(abs(self._runner._pos[0]) > self._lenx/2 or 
-               abs(self._runner._pos[1]) > self._leny/2):
+            if(abs(self._scaper._pos[0]) > self._lenx/2 or 
+               abs(self._scaper._pos[1]) > self._leny/2):
                 print("Out of bounds!")
                 break
-            elif(separation < self._chaser._radius + self._runner._radius):
+            elif(self._chaser.separation(self._scaper) 
+            < self._chaser._radius + self._scaper._radius):
                 print("Caught!")
                 break
             
--- Tests.py
+++ Tests.py
@@ -9,9 +9,18 @@ Early tests for these classes
 
 import Runners as r
 
+#%%
 world1 = r.World(start=
-                 [[0, 0], #runner start
-                 [1, 0], #runner starting velocity
-                 [-1, 1], #chaser start
-                 [-0.8, -0.6]]) #chaser starting velocity
+                 [[4, 3], #runner start
+                 [0, 0], #runner starting velocity
+                 [-1, -1.5], #chaser start
+                 [0, 0]]) #chaser starting velocity
 world1.create()
+
+#%% works, /0 runtime error but who cares
+"""
+c = r.Chaser([1, 1], [0, 0])
+s = r.Scaper([0, 0], [0, -1])
+
+s.isotropic_lines(c, 3)
+"""
